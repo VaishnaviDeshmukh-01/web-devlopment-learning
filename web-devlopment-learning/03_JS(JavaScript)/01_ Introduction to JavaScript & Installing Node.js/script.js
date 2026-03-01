ce8949aa6d5e6c3533f8e4f752fb871caa394ed9
Vaishnavi Deshmukh <deshmukh.vaishnavi1410@gmail.com>
2026-03-01 19:06:30 +0530

    Introduction to JavaScript & Node.js Setup
    - Introduction to JavaScript fundamentals
    - Installing Node.js on different operating systems
    - Verifying Node.js and npm installation
    - Running JavaScript using Node.js
    - Basic project structure setup

---
+++ web-devlopment-learning/03_JS(JavaScript)/01_ Introduction to JavaScript & Installing Node.js/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+console.log("Code is running.....");
+console.log("Code is also running.....");
+console.log("Another code is also running.....");
+
+var a = prompt("Enter your Number");
+var isTrue = confirm("Are you sure to blast your computer")
+if(isTrue){
+console.log("Your computer is blasting")
+}
+else{
+    console.log("Your computer is not blasting")
+}
+
+console.log("Your number is", + a);
+document.title = "Hey I am title";
+document.body.style.backgroundColor ="red";
