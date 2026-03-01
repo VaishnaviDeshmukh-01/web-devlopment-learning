8fe1578330afd5d632913fd33a4c6f843a8233f6
Vaishnavi Deshmukh <deshmukh.vaishnavi1410@gmail.com>
2026-03-01 19:08:27 +0530

    Variables, Data Types, and Objects in JavaScript
    - Variable declarations (var, let, const)
    - Primitive data types (string, number, boolean, null, undefined, symbol, bigint)
    - Reference data types
    - Objects and object properties
    - Basic object methods
    - Type checking and dynamic typing in JavaScript
    
    This serves as a foundational step toward understanding JavaScript programming and backend development with Node.js.

---
+++ web-devlopment-learning/03_JS(JavaScript)/02_JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,44 @@
+console.log("This is website on JavaScript Variables, Data Types & Objects");
+
+// var a = 2;
+// var b = 5;
+// var c = "Harry";
+// var _a = 25
+
+// console.log(_a + b+ 8);
+
+// console.log(typeof a, typeof b, typeof c);
+
+// const a1 = 20;
+// a1 = a1 + 1;
+let a = 2;
+let b = 5;
+let c = "Harry";
+let _a = 25;
+console.log(a);
+{
+    let a = 5;
+    console.log(a);
+}
+
+// Data types
+let x ="Trisha";
+let y = 22;
+let z = 4.33;
+const d = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, d, q, r)
+console.log(typeof x, typeof y, typeof z, typeof d, typeof q, typeof r);
+
+let o ={
+    "name": "trisha",
+    "salary": 50000
+}
+
+console.log(o);
+o.jobrole = "devloper";
+console.log(o);
+o.jobrole = "designer";
+console.log(o);
