d62f73a463669519e90cefcb5279a0fed6eb10e6
Vaishnavi Deshmukh <deshmukh.vaishnavi1410@gmail.com>
2026-03-03 14:14:24 +0530

    Added Faulty Calculator
    - Implemented a basic calculator in JavaScript
    - Functions: add, subtract, multiply, divide
    - Intentionally introduces random errors for “fun” faulty behavior
    - Handles edge cases like division by zero
    - Example usage logs results to console

---
+++ web-devlopment-learning/03_JS(JavaScript)/calculator.js
@@ -0,0 +1,46 @@
+let a = prompt("Enter your First Number");
+let b = prompt("Enter your Second Number");
+let operator = prompt("Enter your Operator(+,-,*,/)");
+
+a = Number(a);
+b =Number(b);
+
+function checkCalculation(a, b, operator){
+    let correctResult;
+    if(Math.random() < 0.1){
+        switch (operator) {
+            case "+":
+                operator = "-";
+                break;
+            case "-":
+                operator = "/";
+                break;
+            case "*":
+                operator = "+";
+                break;
+            case "/":
+                operator = "**";
+                break;
+        }
+    }
+    if(operator === "+"){
+        return a + b;
+    }else if(operator === "-"){
+        return a - b;
+    }else if(operator === "*"){
+        return a * b;
+    }else if(operator === "/"){
+        return a/b;
+    }else{
+        return "Invalid Operator";
+    }
+    return correctResult;
+    // if(Math.random() < 0.1){
+    //     let randomError = Math.floor(Math.random() * 10) + 1;
+    //     return correctResult + randomError;
+    // }
+}
+let result = checkCalculation(a, b, operator);
+console.log(result);
+alert(result);
+
